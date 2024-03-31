c76af8cefb9ab6731244069975384478e14ec5ee
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-04-01 00:44:02 +0100

    Fullstack mern social media app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Flex, Image, useColorMode } from "@chakra-ui/react";
+import { Flex, Image, Button, useColorMode } from "@chakra-ui/react";
+import { CgDarkMode } from "react-icons/cg";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -8,10 +9,24 @@ const Header = () => {
       <Flex justifyContent={"center"} mt={6} mb={12}>
         <Image
           cursor={"pointer"}
-          w={6}
-          alt="Space's Logo"
+          alt="Spacelogo"
+          w={"10"}
           src="/spacelogo.svg"
         />
+
+        <Button
+          h={"fit-content"}
+          w={"fit-content"}
+          padding={"1"}
+          justifySelf={"flex-end"}
+          onClick={toggleColorMode}
+        >
+          {colorMode === "dark" ? (
+            <CgDarkMode className="text-black" size={30} />
+          ) : (
+            <CgDarkMode className="text-white" size={30} />
+          )}
+        </Button>
       </Flex>
     </div>
   );
