14d63d2e9c3ca1b3d62c763fa3e0c597d61134d9
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-04-01 23:50:47 +0100

    Fullstack mern social media app

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -5,12 +5,38 @@ const UserHeader = () => {
     <VStack gap={4} alignItems={"start"}>
       <Flex justifyContent={"space-between"} w={"full"}>
         <Box>
-          <Text fontSize={"2xl"}> Olagunju Emmanuel</Text>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Olagunju Emmanuel
+          </Text>
+          <Flex gap={2} alignItems={"center"}>
+            <Text fontSize={"sm"}>olagunjuemmanuel</Text>
+            <Text
+              fontSize={"xs"}
+              bg={"gray.dark"}
+              color={"gray.light"}
+              p={1}
+              borderRadius={"full"}
+            >
+              relate.net
+            </Text>
+          </Flex>
         </Box>
         <Box>
-          <Avatar name="MLO Olagunju" src="mlo.jpg" size={"xl"} />
+          <Avatar
+            cursor={"pointer"}
+            name="MLO Olagunju"
+            src="mlo.jpg"
+            size={"xl"}
+          />
         </Box>
       </Flex>
+
+      <Text>
+        JavaScript Alchemist | FullStack Engineer | MERNStack Developer | Web
+        Developer | SFC® | AGILE | Theology Scholar
+      </Text>
+
+      <Flex w={"full"}></Flex>
     </VStack>
   );
 };
