4c98caed0d382ea87de618100ec1c08dc144fd51
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-04-03 00:04:37 +0100

    Fullstack mern social media app

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { RiVerifiedBadgeFill } from "react-icons/ri";
+import { Flex, Avatar, Text, Image } from "@chakra-ui/react";
 
 const PostPage = () => {
-  return <div>PostPage</div>;
+  return (
+    <div>
+      <Flex>
+        <Flex w={"full"} alignItems={"center"} gap={3}>
+          <Avatar src="/mlo.jpg" size={"md"} name="Olagunju Emmanuel" />
+          <Flex>
+            <Text fontSize={"small"} fontWeight={"bold"}>
+              mlolagunju
+            </Text>
+            <RiVerifiedBadgeFill className="ml-1 text-sky-600" />
+          </Flex>
+
+          <Flex gap={4} alignItems={"center"}></Flex>
+        </Flex>
+      </Flex>
+    </div>
+  );
 };
 
 export default PostPage;
