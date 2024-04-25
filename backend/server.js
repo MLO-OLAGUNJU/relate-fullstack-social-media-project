293cff39d43eb348bfdb55cd765a2c1d3f57a049
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-04-25 12:24:29 +0100

    Fullstack mern social media app

--- backend/server.js
+++ backend/server.js
@@ -8,5 +8,5 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server don start  at http://localhost:${PORT}`)
+  console.log(`Server don start at http://localhost:${PORT}`)
 );
