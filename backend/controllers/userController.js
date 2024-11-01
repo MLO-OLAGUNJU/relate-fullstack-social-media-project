e5c367d860aa156eeda9b109d16a493fef6b0172
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-11-01 17:43:53 +0100

    Added a console log statement to the logoutUser function in userController.js.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -93,6 +93,7 @@ const logoutUser = (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 1 });
     res.status(200).json({ message: "User logged out successfully" });
+    console.log("User logged out successfully");
   } catch (error) {
     res.status(500).json({ error: error.message });
     console.log("Error in logoutUser:", error.message);
