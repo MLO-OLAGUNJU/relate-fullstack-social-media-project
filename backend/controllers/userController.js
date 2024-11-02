26f8664d8340d0e4d0e3b1816d2ce34926f3631c
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-11-02 10:37:16 +0100

    "Removed console log in logoutUser function and added useState, useEffect, and useParams to UserPage component to fetch user data"

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -93,7 +93,6 @@ const logoutUser = (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 1 });
     res.status(200).json({ message: "User logged out successfully" });
-    console.log("User logged out successfully");
   } catch (error) {
     res.status(500).json({ error: error.message });
     console.log("Error in logoutUser:", error.message);
--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserHeader from "../components/UserHeader";
 import UserPost from "../components/UserPost";
+import { useParams } from "react-router-dom";
 
 const UserPage = () => {
+  const [user, setUser] = useState(null);
+  const { username } = useParams();
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`/api/users/profile/${username}`);
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getUser();
+  }, [username]);
+
   return (
     <div>
       <UserHeader />
