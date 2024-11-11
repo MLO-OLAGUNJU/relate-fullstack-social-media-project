5c732c67bd9624744ced8f22d9aad3b31caaf9b5
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-11-11 17:23:52 +0100

    relate

---
+++ backend/models/conversationModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema(
+  {
+    participants: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+    },
+    lastMessage: {
+      text: {
+        type: String,
+      },
+      sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Conversation = mongoose.model("Conversation", conversationSchema);
+
+export default Conversation;
---
+++ backend/models/messageModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    text: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
