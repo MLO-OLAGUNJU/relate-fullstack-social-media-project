652d1f9bfbe0e8aeb6af8c359c705c9579130dc6
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-11-19 11:35:16 +0100

    "Updated server.js to import and use 'server' from socket.js instead of creating a new express app instance"

---
+++ backend/socket/socket.js
@@ -0,0 +1,18 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:4000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connection established", socket.id);
+});
+
+export { io, server, app };
